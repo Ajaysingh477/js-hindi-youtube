0bae22b0ede7fa0c0f9aa3ab91d3196896e90caf
Ajaysingh477 <160405574+Ajaysingh477@users.noreply.github.com>
2024-06-12 13:27:10 +0000

    conversions of data types

---
+++ basics/conversionandoperation.js
@@ -0,0 +1,29 @@
+let score = "Ajay"
+
+// console.log(typyeof score)
+// console.log(typyeof (score)) // aother way of knowing the typeof data.
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "22" => 22
+//  "22abc" => NaN (not a number) but its a number tpye that sounds bit confusing and it is.
+// true => 1; false => 0
+
+let isLoggedIn = "Ajay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0=> false;
+// " " => false;
+// "Ajay" =>True;
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //  traet all JS code as newer version.
+
+// alert("Hello") // We are using node.js and not browser so alert doesnt work here.
+
+let name = "Ajay"
+let age = 20
+let isLoggedIn = true
+
+// Below all are primitive data type.
+// Number => 2 to the power 53 is the rage of memory address
+// bigint if your number is too big 
+// string => double qoutes " string"
+// boolean => true/false
+// null =>stand aloan valuse with being a data type. null meeans empty and you can also define a value as a null and null is not zero its empty.
+// iunddefined => when a value is not dfined and not assigned and only a var is declared.
+// symbol => whenevr we talk about uniquness.
+
+
+// Object 
+
+console.log(typeof "Ajay")
+console.log(typeof null)// null is an object type as you can see from reult as its displayed that null is object type whereas all other have there own type lliek string,undefined etc.
+console.log(typeof undefined)
